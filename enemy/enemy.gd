9ed7fc8669c5e522cbef4db6b653bb0872db7d29
julian <julian@triband.net>
2024-04-19 23:11:35 +0200

    enemies rotate on Enter/Space

--- enemy/enemy.gd
+++ enemy/enemy.gd
@@ -1,22 +1,22 @@
 extends Node3D
 
-var hp = 0
+var hp:float = 100
 
 @onready() var sprite:Sprite3D = $Sprite3D
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
+# we expecrt a Sprite3D in the tree
 	assert(sprite is Sprite3D)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+# maps to space/enter
 	if Input.is_action_just_pressed("ui_accept"):
 		flip()
 
 func flip():
 	var y = sprite.rotation_degrees.y
 	var tween = create_tween()
-	print(y)
 	if y < 90:
 		tween.tween_property($Sprite3D, "rotation_degrees:y", 90.0, 1)
 	else:	
--- player.gd
+++ player.gd
@@ -1,8 +1,8 @@
 extends PathFollow3D
 
-
+@export var damage: float = 100
+@export var bullets: int = 1
 @export var speed: float = 10.0
-
 
 var player_id: int
 
@@ -13,5 +13,9 @@ var player_id: int
 
 
 func _process(delta: float) -> void:
-    var movement_input: = Input.get_axis(move_left_action, move_right_action)
-    progress += movement_input * speed * delta
+	var movement_input: = Input.get_axis(move_left_action, move_right_action)
+	progress += movement_input * speed * delta
+
+func shoot():
+	# spawn bullet
+	bullets -= 1
