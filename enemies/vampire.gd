288fcc089ac42304d60cd56f49ce8c3690af8dca
Rémi Verschelde <rverschelde@gmail.com>
2024-04-20 21:12:32 +0200

    remove projector modulate, flip vampires

--- enemies/vampire.gd
+++ enemies/vampire.gd
@@ -1,1 +1,9 @@
 extends Enemy
+
+func _process(delta):
+	super(delta)
+	if velocity.x > 0:
+		$AnimatedSprite2D.flip_h = true
+	elif velocity.x < 0:
+		$AnimatedSprite2D.flip_h = false
+	# Keep original flip at 0.
