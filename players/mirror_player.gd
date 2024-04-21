99f77c420178784f18b455e165a22a471046601b
Nikita Blizniuk <bliznyuknm@gmail.com>
2024-04-21 11:02:40 +0200

    jetpack for mirror player

--- players/mirror_player.gd
+++ players/mirror_player.gd
@@ -24,6 +24,8 @@ func _process(delta: float) -> void:
 		Input.get_axis("mirror.move.up", "mirror.move.down")
 	)
 	
+	update_animations(movement, delta)
+	
 	velocity = movement * speed
 	move_and_slide()
 	
@@ -40,10 +42,9 @@ func _process(delta: float) -> void:
 		raycast.target_position = -direction.reflect($MirrorRoot.transform.y) * length
 		
 		var collider = raycast.get_collider()
-		if collider:
+		if collider and collider.has_method("apply_charge"):
 			collider.apply_charge(damage * delta)
 			length = (collider.position - position).length()
-		
 		
 		beam.points[1] = -direction.reflect($MirrorRoot.transform.y) * length
 		
@@ -61,6 +62,13 @@ func unregister_light(light_source) -> void:
 	var beam = light_sources[light_source]
 	beam.queue_free()
 	light_sources.erase(light_source)
+
+
+func update_animations(input: Vector2, delta: float) -> void:
+	if input.length_squared() < 0.0001: $AnimationPlayer.play("hovering")
+	else: $AnimationPlayer.play("flying")
+	input.y *= -1
+	$RedRockets.rotation = lerp_angle($RedRockets.rotation, input.angle_to(Vector2.DOWN), delta * 10)
 
 
 func apply_damage(value: float) -> void:
