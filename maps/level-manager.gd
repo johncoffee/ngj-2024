449c9247afeecd751e401421ad6f91bfbc86a959
Rémi Verschelde <rverschelde@gmail.com>
2024-04-24 12:48:01 +0200

    assorted post-jam fixes
    - prevent projector player from going out of sight
    - add fullscreen toggle in menu
    - tweak some menu positions
    - clarify that gameover might be a win screen ;)
    - remove weird bindings we added to workaround bogus xbox controller mappings
      (upstream bug)

--- maps/level-manager.gd
+++ maps/level-manager.gd
@@ -10,8 +10,10 @@ func _ready():
 	get_tree().create_timer(randf_range(10, 15)).timeout.connect(_on_VampireScream_timeout)
 
 func _on_VampireScream_timeout():
+	# This was a 2-day jam ok?
 	$"../VampireScream".play()
-	get_tree().create_timer(randf_range(10, 20)).timeout.connect(_on_VampireScream_timeout)
+	if is_instance_valid(get_tree()):
+		get_tree().create_timer(randf_range(10, 20)).timeout.connect(_on_VampireScream_timeout)
 
 
 func _handle_enemy_died():
@@ -21,8 +23,8 @@ func _handle_enemy_died():
 
 
 func show_game_over() -> void:
-	EzTransitions.set_easing(0, 1)
-	EzTransitions.set_trans(0, 0)
+	EzTransitions.set_easing(Tween.EASE_IN, Tween.EASE_OUT)
+	EzTransitions.set_trans(Tween.TRANS_LINEAR, Tween.TRANS_LINEAR)
 	EzTransitions.set_timers(1.5, 0, 1.5)
 	EzTransitions.set_reverse(false, false)
 	EzTransitions.set_textures("res://addons/ez_transitions/images/black_texture.png", "res://addons/ez_transitions/images/black_texture.png")
--- ui/main_menu.gd
+++ ui/main_menu.gd
@@ -11,8 +11,22 @@ func start_game():
 	EzTransitions.change_scene("res://maps/sandbox.tscn")
 
 
-func _process(delta: float):
+func _process(_delta: float):
 	if $Projector2.broken:
 		$AnimationPlayer.play("projector_pulsate")
 	else:
 		$AnimationPlayer.stop()
+
+	if Input.is_action_just_pressed("fullscreen"):
+		_on_fullscreen_pressed()
+
+
+func _on_fullscreen_pressed():
+	if get_window().mode == Window.MODE_FULLSCREEN:
+		get_window().mode = Window.MODE_WINDOWED
+		$Fullscreen.text = "Fullscreen (F10)"
+	else:
+		get_window().mode = Window.MODE_FULLSCREEN
+		$Fullscreen.text = "Windowed (F10)"
+	# Release focus so ui_accept doesn't retoggle.
+	$Fullscreen.release_focus()
